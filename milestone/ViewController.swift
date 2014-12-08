ea53cf39deec7eb8001d9fe14aced9ae11fd3956
安田洋介 <double.y.hoihoi@gmail.com>
2014-12-08 11:09:33 +0900

    create project.

---
+++ milestone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  milestone
+//
+//  Created by 安田洋介 on 2014/12/08.
+//  Copyright (c) 2014年 UT-PSI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
