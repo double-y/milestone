05acff98ee49c778e00f82ab4906a3ec433eaeaa
Kota Okada <ksk.so9624@gmail.com>
2014-12-25 22:43:53 +0900

    メインメニューの作成（暫定的）

--- milestone/ViewController.swift
+++ milestone/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var project_menu: UIImageView!
+    
+    @IBOutlet var calender_menu: UIImageView!
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        project_menu.image = UIImage(named:"project")
+        calender_menu.image = UIImage(named: "calender")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        
+    }
 
-
+    
 }
 
